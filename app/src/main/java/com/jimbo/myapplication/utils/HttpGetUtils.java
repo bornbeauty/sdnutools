748ac7611f0d781623511e2b23f997f0c679671f
jimbo <zhongjinbao1994@gmail.com>
2016-04-11 20:14:53 +0800

    new

---
+++ app/src/main/java/com/jimbo/myapplication/utils/HttpGetUtils.java
@@ -0,0 +1,25 @@
+package com.jimbo.myapplication.utils;
+
+import android.os.Handler;
+
+/**
+ * description:
+ *
+ * @author jimbo zhongjinbao1994@gmail.com
+ * @since 2016/4/11 13:13
+ */
+public class HttpGetUtils {
+
+    private String url;
+    private Handler handler;
+
+    public HttpGetUtils(String url, Handler handler) {
+        this.url = url;
+        this.handler = handler;
+    }
+
+    public void get() {
+
+    }
+
+}
